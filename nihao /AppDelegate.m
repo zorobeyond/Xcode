20731d2d71692303b315c15ccafaac2b74dd1ed6
zorobeyond <zorobeyond@163.com>
2019-09-09 15:14:44 +0800

    xcode

---
+++ nihao /AppDelegate.m
@@ -0,0 +1,56 @@
+//
+//  AppDelegate.m
+//  Parameter
+//
+//  Created by admin on 2019/7/23.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    ViewController*vc = [[ViewController alloc]init];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+
+@end
